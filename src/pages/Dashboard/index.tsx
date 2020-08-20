41532a4cc41d20460244dbc75a3ec2e1260ca217
Rafael Santana <rgs1990419@gmail.com>
2020-08-20 13:55:54 -0300

    Tentando retornar pratos da api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -55,11 +55,20 @@ const Dashboard: React.FC = () => {
 
   async function handleNavigate(id: number): Promise<void> {
     // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       // Load Foods from API
+      const response = await api.get<Food[]>('/foods');
+
+      const responseWithFormattedPrice = response.data.map(food => ({
+        ...food,
+        formattedPrice: formatValue(food.price),
+      }));
+
+      setFoods(responseWithFormattedPrice);
     }
 
     loadFoods();
